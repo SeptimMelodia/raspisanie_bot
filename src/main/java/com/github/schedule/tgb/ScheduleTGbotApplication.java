f347170a22a3c10b44a61756b93455ffedf906d5
viktor <stune@student.21-school.ru>
2022-12-04 05:27:51 +0300

    add project scileton

---
+++ src/main/java/com/github/schedule/tgb/ScheduleTGbotApplication.java
@@ -0,0 +1,13 @@
+package com.github.schedule.tgb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleTGbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleTGbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/schedule/tgb/ScheduleTGbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.schedule.tgb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleTGbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
